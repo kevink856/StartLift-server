23ae87a48455aa6592b8c9419b3cb58d930c0782
Kevin Kim <kevink856@gmail.com>
2023-07-06 14:23:55 -0400

    Add Endpoints for Forgot Password
    Added GET and POST request endpoints to validate email and reset password

--- routes/main.js
+++ routes/main.js
@@ -56,4 +56,44 @@ router.post("/login", async (req, res) => {
     }
 });
 
+//Handle Forgot Password Email Request
+router.get("/forgot", async (req, res) => {
+    const email = req.query.email;
+    if(!validator.validate(email)) {
+        res.send("bademail");
+    } else {
+        pool.query({ text: "SELECT * FROM users WHERE email = $1",
+            values: [email], }, (error, response) => {
+            if(response.rows.length > 0) {
+                res.send("success");
+            } else {
+                res.send("fail");
+            }
+        });
+    }
+});
+
+//Handle Forgot Password Request
+router.post("/forgot", async (req, res) => {
+    if(!validator.validate(req.body.email)) {
+        res.send("bademail");
+    } else {
+        pool.query({ text: "SELECT * FROM users WHERE email = $1",
+            values: [req.body.email], }, (error, response) => {
+            if(response.rows.length > 0) {
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(req.body.password, salt, (err, hash) => {
+                        pool.query("UPDATE users SET password = $1 WHERE email = $2)",
+                            [hash, req.body.email], (error, response) => {
+                            res.send("success");
+                        });
+                    });
+                });
+            } else {
+                res.send("fail");
+            }
+        });
+    }
+});
+
 module.exports = router;
